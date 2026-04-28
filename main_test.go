233d63d3ab0d7d9f57d0f05b95399e983b3baa11
RomG <romagureev170@gmail.com>
2026-04-29 00:11:50 +0800

    Added 2 new tests

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCafeNegative(t *testing.T) {
@@ -46,5 +48,81 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		count int
+		want  int
+	}{
+		{count: 0, want: 0},
+		{count: 1, want: 1},
+		{count: 2, want: 2},
+		{count: 100, want: len(cafeList["moscow"])},
+	}
+
+	for _, req := range requests {
+		response := httptest.NewRecorder()
+
+		request := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/cafe?city=moscow&count=%d", req.count), nil)
+
+		handler.ServeHTTP(response, request)
+
+		require.Equal(t, http.StatusOK, response.Code)
+
+		body := response.Body.String()
+
+		var cafes []string
+		if body == "" {
+			cafes = []string{}
+		} else {
+			cafes = strings.Split(body, ",")
+		}
+
+		assert.Equalf(t, req.want, len(cafes), "count=%d", req.count)
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		search    string
+		wantCount int
+	}{
+		{search: "фасоль", wantCount: 0},
+		{search: "кофе", wantCount: 2},
+		{search: "вилка", wantCount: 1},
+	}
+
+	for _, req := range requests {
+		response := httptest.NewRecorder()
+
+		request := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/cafe?city=moscow&search=%s", req.search), nil)
+
+		handler.ServeHTTP(response, request)
+
+		require.Equal(t, http.StatusOK, response.Code)
+
+		body := response.Body.String()
+
+		var cafes []string
+		if body == "" {
+			cafes = []string{}
+		} else {
+			cafes = strings.Split(body, ",")
+		}
+
+		assert.Equalf(t, req.wantCount, len(cafes), "search=%q", req.search)
+
+		for _, cafe := range cafes {
+			cafe = strings.ToLower(cafe)
+			search := strings.ToLower(req.search)
+
+			assert.True(t, strings.Contains(cafe, search))
+		}
 	}
 }
